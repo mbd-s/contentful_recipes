73e0dde893c4c2dd99cef67a9d178670dc6f4ff4
Michael Silverberg <michaelbsilverberg@gmail.com>
2020-09-16 14:01:17 +0200

    Handle unknown routes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,6 +3,7 @@
 class RecipesController < ApplicationController
   include ApplicationHelper
   before_action :set_recipes, only: %i[index]
+  around_action :catch_not_found
   before_action :set_recipe, only: %i[show]
 
   def index
@@ -26,5 +27,11 @@ class RecipesController < ApplicationController
     recipe_fields_collection.each do |fields_collection|
       RecipeBuilder.new(fields_collection).perform
     end
+  end
+
+  def catch_not_found
+    yield
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@
 Rails.application.routes.draw do
   root 'recipes#index'
   resources :recipes, only: %i[index show]
+  match '*path', to: 'recipes#index', via: :all
 end
